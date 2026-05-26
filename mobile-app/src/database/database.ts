9d199d5ce74991441601e66feb516c7cf205f2d5
Karan <aanvinegi06@gmail.com>
2026-05-26 19:52:46 +0530

    add

--- mobile-app/src/database/database.ts
+++ mobile-app/src/database/database.ts
@@ -10,3 +10,49 @@ const db = SQLite.openDatabase(
 )
 
 export default db
+export function insertUser(
+  name: string,
+  embedding: string
+) {
+
+  db.transaction(tx => {
+
+    tx.executeSql(
+
+      `
+      INSERT INTO users
+      (name, embedding)
+     
+      VALUES (?, ?)
+      `,
+
+      [name, embedding],
+
+      () => {
+        console.log('User Saved')
+      },
+
+      error => {
+        console.log(error)
+      }
+    )
+  })
+}
+export function initializeDatabase() {
+
+  db.transaction(tx => {
+
+    tx.executeSql(
+
+      `
+      CREATE TABLE IF NOT EXISTS users (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT,
+        embedding TEXT
+      );
+      `
+
+    )
+
+  })
+}
